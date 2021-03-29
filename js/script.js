2661f2b417f614ea9d7a227921795fbf875f7ab6
enrico bianco <biancoenrico71@gmail.com>
2021-03-29 19:24:32 +0200

    completata logica pagina

--- js/script.js
+++ js/script.js
@@ -10,6 +10,52 @@
 
 var sendButton = document.getElementById('send-buton')
 var deleteButton = document.getElementById('delete-button')
-var nomeUtente = document.getElementById('nome-utente')
 
-sendButton.addEventListener('click')
+sendButton.addEventListener('click',function(){
+    var costoKm = 0.21;
+    var chilometriNum = parseInt(document.getElementById('km-da-percorrere').value);
+    var prezzoBiglietto = (chilometriNum * costoKm );
+    var prezzofinale
+
+    var nomeUtente = document.getElementById('nome-utente').value;
+    var eta = document.getElementById('etá').value
+
+    if( eta == 'minorenne' ){
+        prezzofinale = prezzoBiglietto - prezzoBiglietto * 0.20
+    }else if( eta == 'over65'){
+        prezzofinale = prezzoBiglietto - prezzoBiglietto * 0.40
+    }else{
+        prezzofinale = prezzoBiglietto
+    }
+
+    document.getElementById('biglietto').className = '';
+
+    document.getElementById('nome').innerHTML = nomeUtente
+    console.log(document.getElementById('nome').innerHTML );
+
+    document.getElementById('offerta').innerHTML = eta
+    document.getElementById('carrozza').innerHTML = getRndInteger(1,10)
+    document.getElementById('codice').innerHTML = getRndInteger(90000,99999)
+    document.getElementById('costo').innerHTML = prezzofinale + ' $'
+
+})
+deleteButton.addEventListener('click',function(){
+ 
+    document.getElementById('biglietto').className = 'void';
+    azzera('nome-utente');
+    azzera('km-da-percorrere')
+    azzera('etá')
+    azzera('nome')
+    azzera('offerta')
+})
+
+
+
+
+
+function azzera(id) { //funzione che attiva il cambio del testo
+    document.getElementById(id).value = ''
+}
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
